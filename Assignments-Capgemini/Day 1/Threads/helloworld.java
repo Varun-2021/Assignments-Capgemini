d30a60c60502ae85bd2aba1da85663a1be30e5a3
varun <varunchidagam008@gmail.com>
2022-02-07 11:13:43 +0530

    thread demo

---
+++ Assignments-Capgemini/Day 1/Threads/helloworld.java
@@ -0,0 +1,21 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str= "Hello Welcome to Java World!!", nstr="";
+        char ch;
+       
+     
+       
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i); //extracts each character
+        nstr= ch+nstr; //adds each character in front of the existing string
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+
+	}
+
+
